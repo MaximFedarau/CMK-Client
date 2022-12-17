f5fc107877e25fa6139b27db5708d59c64b940a0
maxim <fedaraumaxim@gmail.com>
2022-12-17 23:29:44 +0300

    feat: update App config 1) Uninstall @react-native-community/eslint-config & change it to custom config. 2) Install babel-plugin-module-resolver & add path aliases.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        alias: {
+          'components': './src/components',
+          'constants': './src/constants',
+          'screens': './src/screens',
+          'navigation': './src/navigation',
+          'types': './src/types',
+          'utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
