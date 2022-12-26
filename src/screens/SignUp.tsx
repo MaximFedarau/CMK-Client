21a741b723dc9d639ab8f0687d786467ad1f0492
maxim <fedaraumaxim@gmail.com>
2022-12-26 20:18:47 +0300

    feat: SignUp screen initial setup Add Logo & AuthFormButtons components.

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,26 @@
 import React, { FC } from 'react';
 
-export const SignUp: FC = () => <></>;
+import {
+  ContentScrollContainer,
+  Logo,
+  FormContentContainer,
+  AuthFormButtons,
+} from '@components';
+import { NavigationAuthName } from '@types';
+
+export const SignUp: FC = () => {
+  const onSubmit = () => {
+    console.log('onSubmit');
+  };
+
+  return (
+    <ContentScrollContainer>
+      <Logo />
+      <FormContentContainer>
+        <AuthFormButtons type={NavigationAuthName.SIGN_UP} onSubmit={onSubmit}>
+          Next
+        </AuthFormButtons>
+      </FormContentContainer>
+    </ContentScrollContainer>
+  );
+};
